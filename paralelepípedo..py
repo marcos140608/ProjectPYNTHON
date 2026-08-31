3b3b988bb6f1ca529d45d44af603721c5403022f
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-08-31 09:41:46 -0300

    Add files via upload

---
+++ paralelepípedo..py
@@ -0,0 +1,5 @@
+comprimento=float(input("Informe o comprimento: "))
+largura=float(input("Informe o largura: "))
+altura=float(input("Informe o altura: "))
+volume=comprimento*largura*altura
+print(f"O volume do troca:{volume} ")
