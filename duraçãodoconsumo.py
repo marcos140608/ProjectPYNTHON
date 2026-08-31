3595789af7ab14eda096a02a22d04ae8f71272a5
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-08-31 11:03:22 -0300

    Add files via upload

---
+++ duraçãodoconsumo.py
@@ -0,0 +1,4 @@
+quantidadedealimentos=float(input("Qual é a quantidade de alimento em kg:"))
+consumodiario=0.05
+duracao=quantidadedealimentos/consumodiario
+print(f"a quantidade de alimento vai durar por: {duracao} dias")
