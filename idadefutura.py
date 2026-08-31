a4b3fed530f50d4cf95c21818f5fbfb1f0a3b304
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-08-31 10:49:39 -0300

    Add files via upload

---
+++ idadefutura.py
@@ -0,0 +1,5 @@
+anodenascimento=float(input("Digite o ano de nascimento: "))
+anoatual=float(input("Digite o ano atual: "))
+idadeatual=anoatual-anodenascimento
+idadefutura=idadeatual+17
+print(f"minha idade daqui 17 anos sera: {idadefutura}")
