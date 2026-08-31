df7727cc2e3a48b3b3d4238f3f32b0744e45ae73
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-08-31 09:47:31 -0300

    Add files via upload

---
+++ rendimentoaummes.py
@@ -0,0 +1,4 @@
+deposito=float(input("digite o valor do deposito: "))
+rendimentoames=float(input("digite o valor do rendimento a mes:"))
+rendimento=deposito + deposito*rendimentoames/100
+print(f"o rendimento apos um mes sera: {rendimento}")
