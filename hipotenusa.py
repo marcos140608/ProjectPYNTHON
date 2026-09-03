5123ff522d109242705e209f914ffde3e9a62e53
Marcos Vinicius Da Silva Leocadio <115330239+marcos140608@users.noreply.github.com>
2026-09-03 11:17:29 -0300

    Add files via upload

---
+++ hipotenusa.py
@@ -0,0 +1,4 @@
+cateto1=float(input("digite o valor do cateto 1:"))
+cateto2=float(input("digite o valor do cateto 2:"))
+hipotenusa=(cateto1**2+cateto2**2)**0.5
+print(f"o valor da hipotenusa sera: {hipotenusa}")
